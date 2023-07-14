ec8912635c7e064f6e6144a86d4345f236f6b313
wudajian <wudajian@mistong.com>
2023-07-14 15:55:32 +0800

    ✨ feat: 添加 getCurrentEnvironment

---
+++ src/getCurrentEnvironment.ts
@@ -0,0 +1,26 @@
+enum Environment {
+  Browser = 'browser',
+  Node = 'node',
+  Unknown = 'unknown',
+}
+
+/**
+ * @description 判断当前的运行环境
+ * @author wudajian
+ * @date 2023/07/12 10:07:47
+ * @export
+ * @returns {*}  {Environment}
+ */
+export function getCurrentEnvironment(): Environment {
+  if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+    return Environment.Browser;
+  } else if (typeof process !== 'undefined' && process.release && process.release.name === 'node') {
+    return Environment.Node;
+  } else {
+    return Environment.Unknown;
+  }
+}
+
+// 示例用法
+// const currentEnvironment = getCurrentEnvironment();
+// console.log('当前环境:', currentEnvironment);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { getCurrentEnvironment } from './getCurrentEnvironment.js';
+
 export { _copy } from './_copy.js';
 
-console.log('js-utils version 1.1.0');
+console.log('js-utils version 1.1.0', '当前环境:', getCurrentEnvironment());
